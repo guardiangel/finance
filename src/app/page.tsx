4fba968f75f51fb2bf0419cd19717867408fbb45
guardiangel <felix.sgq@gmail.com>
2024-09-07 22:40:07 -0400

    Add theme and nav bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,23 @@
+"use client"
+import { ThemeProvider,CssBaseline, Box } from "@mui/material";
+import { createTheme} from "@mui/material/styles";
+import { useMemo } from "react";
+import { themeSettings } from "./theme";
+import NavBar from "@/pages/navbar/index";
 
 export default function Home() {
+  const theme = useMemo(() => 
+     createTheme(themeSettings)
+  ,[])
   return (
     <div className="app">
-      aa
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+        <NavBar/>
+        aaaaaa
+        </Box>
+        </ThemeProvider>
     </div>
   );
 }
